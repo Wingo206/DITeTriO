c4a5c039529ada22ad59a42407ecc508f132e917
Wingo206 <50254747+Wingo206@users.noreply.github.com>
2024-11-03 19:43:50 -0500

    project setup for c sharp components

---
+++ DITeTriO_cs/DITeTriOPreProcessor/Program.cs
@@ -0,0 +1,7 @@
+﻿using TetrEnvironment.Constants;
+using TetrLoader;
+using TetrLoader.Enum;
+using Environment = TetrEnvironment.Environment;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
